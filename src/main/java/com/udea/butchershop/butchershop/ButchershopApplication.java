b17b06202d031243eac6acf1e14c8729b8420153
Ramiro Monroy Ramos <ramiro.monroy@udea.edu.co>
2020-05-14 14:10:00 -0500

    Initial commit

---
+++ src/main/java/com/udea/butchershop/butchershop/ButchershopApplication.java
@@ -0,0 +1,13 @@
+package com.udea.butchershop.butchershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButchershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButchershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/butchershop/butchershop/ButchershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.butchershop.butchershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButchershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
