65f2ef276be3fd329fa59f1a011d006045f5a909
Ramiro Monroy Ramos <ramiro.monroy@udea.edu.co>
2020-05-14 14:56:27 -0500

    Creating the entity class

---
+++ src/main/java/com/udea/butchershop/butchershop/entity/Customer.java
@@ -0,0 +1,112 @@
+package com.udea.butchershop.butchershop.entity;
+
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name = "v1/Customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column
+    private String name;
+    
+    @Column
+    private String lastname;
+    
+    @Column
+    private String address;
+    
+    @Column
+    private String telephone;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Customer customer = (Customer) o;
+
+        return id != null ? id.equals(customer.id) : customer.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
